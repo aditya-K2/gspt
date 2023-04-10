5f37515c479221537b955550d05cae070a74effa
aditya-K2 <adityakurdunkar2@gmail.com>
2023-04-10 09:45:26 +0530

    remove AddAlbumToPlaylist

--- spt/add.go
+++ spt/add.go
@@ -2,19 +2,6 @@ package spt
 
 import "github.com/zmb3/spotify/v2"
 
-func AddAlbumToPlaylist(albumId, playlistId spotify.ID) error {
-	a, err := GetAlbum(albumId)
-	if err != nil {
-		return err
-	}
-	t := []spotify.ID{}
-	for _, v := range (*a).Tracks {
-		t = append(t, v.ID)
-	}
-	_, err = Client.AddTracksToPlaylist(ctx(), playlistId, t...)
-	return err
-}
-
 func AddTracksToPlaylist(playlistId spotify.ID, t ...spotify.ID) error {
 	_, err := Client.AddTracksToPlaylist(ctx(), playlistId, t...)
 	return err
